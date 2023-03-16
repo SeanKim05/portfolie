c2c1d07fb47991ce383838fc8fd7485e003b7ea1
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2023-03-17 01:15:43 +0900

    [change] Idoframe mobile bg height

--- src/components/MyNameComp/MyNameFrame.js
+++ src/components/MyNameComp/MyNameFrame.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import classes from './MyNameFrame.module.scss';
 import { useNavigate } from 'react-router-dom';
 
