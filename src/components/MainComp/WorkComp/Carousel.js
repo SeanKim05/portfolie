6d77f11f4375b4373cc5cb60727c2bfb5887e0da
SeanKim05 <dbgus20609@gmail.com>
2025-02-19 11:43:33 +0900

    Fix: 캐러셀 튐 현성  수정

--- src/components/MainComp/WorkComp/Carousel.js
+++ src/components/MainComp/WorkComp/Carousel.js
@@ -4,42 +4,38 @@ import classes from "./Carousel.module.scss";
 const Carousel = ({ project_data }) => {
   return (
     <section className={classes.carousel_container}>
-      <div className={classes.slider}>
-        <div className={classes.slide_track}>
-          {project_data.map((data) => (
-            <div className={classes.slide} key={data.id}>
-              <div className={classes.slide_img_wrapper}>
-                <img
-                  className={classes.slide_img}
-                  src={require(`../../../assets/images/${
-                    project_data[data.id].img_name
-                  }`)}
-                  alt={data.title}
-                />
-                <p className={classes.slide_img_text}>
-                  <span>@&nbsp;{data.title}</span>
-                </p>
-              </div>
+      <div className={classes.slide_track}>
+        {/* 원본 슬라이드 목록 */}
+        {project_data.map((data) => (
+          <div className={classes.slide} key={data.id}>
+            <div className={classes.slide_img_wrapper}>
+              <img
+                className={classes.slide_img}
+                src={require(`../../../assets/images/${data.img_name}`)}
+                alt={data.title}
+              />
+              <p className={classes.slide_img_text}>
+                <span>@ {data.title}</span>
+              </p>
             </div>
-          ))}
-          {/* double */}
-          {project_data.map((data) => (
-            <div className={classes.slide} key={data.id}>
-              <div className={classes.slide_img_wrapper}>
-                <img
-                  className={classes.slide_img}
-                  src={require(`../../../assets/images/${
-                    project_data[data.id].img_name
-                  }`)}
-                  alt={data.title}
-                />
-                <p className={classes.slide_img_text}>
-                  <span>@&nbsp;{data.title}</span>
-                </p>
-              </div>
+          </div>
+        ))}
+
+        {/* 동일 슬라이드 목록 반복 */}
+        {project_data.map((data) => (
+          <div className={classes.slide} key={`duplicate-${data.id}`}>
+            <div className={classes.slide_img_wrapper}>
+              <img
+                className={classes.slide_img}
+                src={require(`../../../assets/images/${data.img_name}`)}
+                alt={data.title}
+              />
+              <p className={classes.slide_img_text}>
+                <span>@ {data.title}</span>
+              </p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </section>
   );
